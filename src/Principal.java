25ebbf6db12b8f714f3cb5bf772e77d7932c91a6
quindio <ing.nicolaspr@gmail.com>
2019-11-26 14:15:23 -0500

    actualizacion del .class

--- src/Principal.java
+++ src/Principal.java
@@ -1,1 +1,6 @@
-import javax.swing.JOptionPana; public class Principal{public  static void prueba(){}}
+import javax.swing.JOptionPane;
+ public class Principal {
+ 	public static void main(String[] args) {} 
+	public  static void prueba(){}
+	public  static int suma(int a, int b){}
+}
