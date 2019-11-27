b63412eb30cdb7ec4657229b7c615ef7e7e75ea2
quindio <ing.nicolaspr@gmail.com>
2019-11-26 19:21:11 -0500

    actualizacion del archivo

--- src/Principal.java
+++ src/Principal.java
@@ -1,6 +1,5 @@
 import javax.swing.JOptionPane;
  public class Principal {
  	public static void main(String[] args) {} 
-	public  static void prueba(){}
-	public  static int suma(int a, int b){}
+	public  static void prueba(){inta;a=25;a=3;}
 }
